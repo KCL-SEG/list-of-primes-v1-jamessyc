8956af1aa034b7b40600818e6cf4e46b1adaee5a
James Coward <james.sy.coward@gmail.com>
2023-10-02 22:54:12 +0100

    Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    i = 3
+    while i and len(list) < number_of_primes:
+        if is_prime(i):
+            list.append(i)
+        i += 1        
     return list
+
+def is_prime(num):
+    for i in range(1, num):
+        if num % i == 0 and i != num and i != 1:
+            return False
+    return True
